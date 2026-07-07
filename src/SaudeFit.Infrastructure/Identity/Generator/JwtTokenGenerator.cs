1bc02f44f3a794d0cac8a0a6f6a427e8a8bc9aa4
Mikael Santos <mykaelsantos@outlook.com.br>
2026-07-07 01:39:41 -0300

    register di and refactor local

--- src/SaudeFit.Infrastructure/Identity/Generator/JwtTokenGenerator.cs
+++ src/SaudeFit.Infrastructure/Identity/Generator/JwtTokenGenerator.cs
@@ -7,7 +7,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace SaudeFit.Infrastructure.Generator;
+namespace SaudeFit.Infrastructure.Identity.Generator;
 
 public class JwtTokenGenerator : ITokenGenerator
 {
@@ -18,7 +18,7 @@ public class JwtTokenGenerator : ITokenGenerator
         _jwtSettings = jwtOptions.Value;
     }
 
-    public Application.Common.AuthResponse GenerateToken(AuthenticatedUser user)
+    public AuthResponse GenerateToken(AuthenticatedUser user)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.UTF8.GetBytes(_jwtSettings.Key);
@@ -37,6 +37,6 @@ public class JwtTokenGenerator : ITokenGenerator
         };
 
         var token = tokenHandler.CreateToken(tokenDescriptor);
-        return new Application.Common.AuthResponse(tokenHandler.WriteToken(token), tokenDescriptor.Expires!.Value);
+        return new AuthResponse(tokenHandler.WriteToken(token), tokenDescriptor.Expires!.Value);
     }
 }
---
+++ src/SaudeFit.Infrastructure/Identity/Services/IdentityService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SaudeFit.Application.Abstractions;
+using SaudeFit.Application.Common;
+
+namespace SaudeFit.Infrastructure.Identity.Services;
+
+public class IdentityService : IIdentityService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public IdentityService(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<AuthenticatedUser?> ValidateCredentialsAsync(string email, string password)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+        if (user is null || !await _userManager.CheckPasswordAsync(user, password))
+            return null;
+
+        return new AuthenticatedUser(user.Id, user.UserName!);
+    }
+
+    public async Task<(bool Succeeded, IEnumerable<string> Errors)> RegisterAsync(string email, string password)
+    {
+        var user = new ApplicationUser { UserName = email, Email = email };
+        var result = await _userManager.CreateAsync(user, password);
+        return (result.Succeeded, result.Errors.Select(e => e.Description));
+    }
+}
