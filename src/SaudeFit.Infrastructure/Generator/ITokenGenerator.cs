15784c759fd8a152c2454f9ede174e7a3111b9e7
Mikael Santos <mykaelsantos@outlook.com.br>
2026-07-07 01:02:53 -0300

    refactor config jwt and add generator

---
+++ src/SaudeFit.Infrastructure/Generator/ITokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using SaudeFit.Infrastructure.Identity;
+using SaudeFit.Infrastructure.Identity.DTOs;
+
+namespace SaudeFit.Infrastructure.Generator;
+
+public interface ITokenGenerator
+{
+    AuthResponse GenerateToken(ApplicationUser user);
+}
